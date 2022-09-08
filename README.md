67632d992b65be9d41095411460f68b90a1c84b8
ravi <ravitokas.9000@gmail.com>
2022-09-09 03:30:09 +0400

    added Routing

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # MEARN AMAZONA
 
 # Lessons
+
 1. Introduction
 2. Install Tools
 3. Create react app
 4. create git repository
 5. List Products
-    1. Create product array
-    2. add product image
-    3. render products
-    4. style products
-    
+   1. Create product array
+   2. add product image
+   3. render products
+   4. style products
+6. Add Routing
+   1. npm i react-router-dom
+   2. create route for home screen
+   3. create router for product screen
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,23 @@
 import "./App.css";
-import data from "./data";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a href="/">Amazona </a>
-      </header>
-      <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div key={product.slug} className="product">
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/">Amazona </Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
