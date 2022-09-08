2d3d595df525b85510df08786b0838ed85d1b503
ravi <ravitokas.9000@gmail.com>
2022-09-09 02:27:36 +0400

    First commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="#">Amazona </a>
+      </header>
+      <main>List productscd .</main>
+    </div>
+  );
+}
+
+export default App;
