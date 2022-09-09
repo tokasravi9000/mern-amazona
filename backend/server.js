b1117d920a29c3dd43928b7c1f2d813058492fb6
ravi <ravitokas.9000@gmail.com>
2022-09-09 04:00:12 +0400

    added backend server express

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import data from "./data.js";
+const app = express();
+
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
