6f588eab46dcdc1493e44403f62ab3e05ad17257
ravi <ravitokas.9000@gmail.com>
2022-09-09 17:18:18 +0400

    axios connected

--- README.md
+++ README.md
@@ -6,16 +6,22 @@
 2. Install Tools
 3. Create react app
 4. create git repository
+
 5. List Products
+
    1. Create product array
    2. add product image
    3. render products
    4. style products
+
 6. Add Routing
+
    1. npm i react-router-dom
    2. create route for home screen
    3. create router for product screen
+
 7. Create Node.JS Server
+
    1. run npm init in root folder
    2. Update package.json set type: module
    3. Add .js to imports
@@ -28,3 +34,11 @@
    10. create route for /api/products
    11. return products
    12. run npm start
+
+8. Fetch Products From Backend
+
+   1. set proxy in package.json
+   2. npm install axios
+   3. use state hook
+   4. use effect hook
+   5. use reducer hook
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,28 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import data from "../data";
+import axios from "axios";
+//import data from "../data";
+import "bootstrap/dist/css/bootstrap.css";
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 
 const HomeScreen = () => {
+  const [products, setproducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get("/api/products");
+      setproducts(result.data);
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      <h1>Featured Products</h1>
-      <div className="products">
-        {data.products.map((product) => (
-          <div key={product.slug} className="product">
-            <Link to={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name} />
-            </Link>
-            <div className="product-info">
-              <Link to={`/product/${product.slug}`}>
-                <p>{product.name}</p>
-              </Link>
-              <p>
-                <strong>${product.price}</strong>
-              </p>
-              <button>Add to cart</button>
-            </div>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <h1>Featured Products</h1>
+          <div className="products">
+            {products.map((product) => (
+              <div key={product.slug} className="product text-center">
+                <Link to={`/product/${product.slug}`}>
+                  <img src={product.image} alt={product.name} />
+                </Link>
+                <div className="product-info">
+                  <Link
+                    to={`/product/${product.slug}`}
+                    className="product__Title"
+                  >
+                    <p>{product.name}</p>
+                  </Link>
+                  <p className="product__price">
+                    <strong>${product.price}</strong>
+                  </p>
+                  <button className="btn btn-primary">Add to cart</button>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     </div>
   );
