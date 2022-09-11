9d7f2d36f22129973f2b4f8cd08f10070a3b501d
ravi <ravitokas.9000@gmail.com>
2022-09-11 18:26:57 +0400

    Create Loading and Message Component

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,19 @@ import express from "express";
 import data from "./data.js";
 const app = express();
 
+// product listing
 app.get("/api/products", (req, res) => {
   res.send(data.products);
+});
+
+// Product detail page
+app.get("/api/products/slug/:slug", (req, res) => {
+  const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product not found" });
+  }
 });
 
 const port = process.env.PORT || 4000;
