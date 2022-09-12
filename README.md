36411cd54a7c51835123384e891c226d83bd9bf2
ravi <ravitokas.9000@gmail.com>
2022-09-12 16:22:07 +0400

    Create React Context For Add Item To Cart

--- README.md
+++ README.md
@@ -55,14 +55,25 @@
     2. update App.js
 
 11. Create Product and Rating Component
+
     1. create Rating component
     2. Create Product component
     3. Use Rating component in Product component
+
 12. Create Product Details Screen
+
     1. fetch product from backend
     2. create 3 columns for image, info and action
+
 13. Create Loading and Message Component
+
     1. create loading component
     2. use spinner component
     3. craete message component
     4. create utils.js to define getError fuction
+
+14. Create React Context For Add Item To Cart
+    1. Create React Context
+    2. define reducer
+    3. create store provider
+    4. implement add to cart button click handler
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -12,6 +12,8 @@ import { Helmet } from "react-helmet-async";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
 import { getError } from "../utils";
+import { useContext } from "react";
+import { Store } from "../Store";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -49,6 +51,15 @@ const ProductScreen = () => {
     };
     fetchData();
   }, [slug]);
+
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const addToCartHandler = () => {
+    ctxDispatch({
+      type: "CART_ADD_ITEM",
+      payload: { ...product, quantity: 1 },
+    });
+  };
+
   return loading ? (
     <LoadingBox />
   ) : error ? (
@@ -104,7 +115,9 @@ const ProductScreen = () => {
                 {product.countInStock > 0 && (
                   <ListGroup.Item>
                     <div className="d-grid">
-                      <Button variant="primary">Add to cart</Button>
+                      <Button variant="primary" onClick={addToCartHandler}>
+                        Add to cart
+                      </Button>
                     </div>
                   </ListGroup.Item>
                 )}
