c72977874555229b0d454aca425e6fd91c77848e
ravi <ravitokas.9000@gmail.com>
2022-11-01 13:25:26 +0530

    rendom commit

--- README.md
+++ README.md
@@ -73,7 +73,10 @@
     4. create utils.js to define getError fuction
 
 14. Create React Context For Add Item To Cart
+
     1. Create React Context
     2. define reducer
     3. create store provider
     4. implement add to cart button click handler
+
+15.
--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,15 @@ app.get("/api/products/slug/:slug", (req, res) => {
   }
 });
 
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id == req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product not found" });
+  }
+});
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
